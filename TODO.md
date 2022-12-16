347e95bdb878a619081113d02307769007031d23
Karol <kurekdev@gmail.com>
2022-12-16 17:15:46 +0100

    first commit

---
+++ TODO.md
@@ -0,0 +1,11 @@
+# Issues / TODO
+2. PostgreSQL database instead of sqlite
+3. Direct Messages Frontend
+4. DM with WebSockets (Django Channels + Redis)
+5. Stories logic
+6. 1/2 images are not displaying correctly
+7. Google, Facebook auth todo
+8. Email token (register/reset password)
+9. Edit profile (remove required fields and avatar upload issue)
+10. Black theme works only in index.html
+11. Messages are duplicated
---
+++ config/urls.py
@@ -0,0 +1,39 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from authy.views import UserProfile, follow
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('authy.urls')),
+    path('', include('post.urls')),
+    path('message/', include('directs.urls')),
+    path('notifications/', include('notification.urls')),
+
+    # profile
+    path('<username>/', UserProfile, name='profile'),
+    path('<username>/saved/', UserProfile, name='profilefavourite'),
+    path('<username>/follow/<option>/', follow, name='follow'),
+
+]
+
+# This is used for
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
